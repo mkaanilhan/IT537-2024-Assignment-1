ef4163f681a05c43b2c4b449b387811e0c48ed1a
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-06-21 18:52:47 +0300

    custom element added

---
+++ constant.js
@@ -0,0 +1,56 @@
+const info = [
+  {
+    icon: "folder",
+    textOne: ".github/workflows",
+    textTwo: "Add Shopify Oxygen deployment workflow file",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "folder",
+    textOne: "app",
+    textTwo: "reverted entry.client.jsx",
+    textThree: "1 months ago",
+  },
+  {
+    icon: "folder",
+    textOne: "public",
+    textTwo: "Add index.html",
+    textThree: "3 months ago",
+  },
+  {
+    icon: "file",
+    textOne: ".eslintignore",
+    textTwo: ".DS_Store added to the gitignore",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: ".eslintrc.js",
+    textTwo: "Add index.js",
+    textThree: "6 months ago",
+  },
+  {
+    icon: "file",
+    textOne: ".gitignore",
+    textTwo: "Add LICENSE",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: ".graphqlrc.yml",
+    textTwo: "Add README",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "index.html",
+    textTwo: "Add index.html",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "style.css",
+    textTwo: "Add style.css",
+    textThree: "5 months ago",
+  },
+];
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-});
+document.addEventListener("DOMContentLoaded", function () {
+  class TableItem extends HTMLElement {
+    constructor(textOne = "Test", textTwo = "Test", textThree = "Test") {
+      super();
+      this.textOne = textOne;
+      this.textTwo = textTwo;
+      this.textThree = textThree;
 
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.innerHTML = `
+  
+                    <div class="files-table__item">
+
+                        <span>  <i data-lucide="folder"></i> <span> ${this.textOne} </span>   </span>
+                        <span> ${this.textTwo} </span>
+                        <span> ${this.textThree} </span>
+                    
+                    <div/>
+        `;
+
+        connectedCallback() {
+          this.shadowRoot.querySelector("i").setAttribute("data-lucide", this.icon);
+        };
+    }
+  }
+
+  customElements.define("table-item", TableItem);
+
+  const el = document.querySelector(".files-table");
+  info.forEach((item) => {
+    el.appendChild(new TableItem(item.textOne, item.textTwo, item.textThree));
+  });
+  el.appendChild(new TableItem());
+});
