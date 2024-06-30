fc4bc0efe7042e9f81fdc3adf95fc54110522566
Umut Dündar <umutdundar@Umuts-MacBook-Pro.local>
2024-06-30 14:50:55 +0300

    uaDundar commit

---
+++ constant/constant.js
@@ -0,0 +1,50 @@
+const info = [
+  {
+    icon: "folder",
+    textOne: "app",
+    textTwo: "version 1",
+    textThree: "1 months ago",
+  },
+  {
+    icon: "folder",
+    textOne: "public",
+    textTwo: "Add index.html",
+    textThree: "3 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "src",
+    textTwo: "Changed the base URL of the API.",
+    textThree: "4 months ago",
+  },
+  {
+    icon: "file",
+    textOne: ".gitignore",
+    textTwo: "added UI for signup & signin",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "README.md",
+    textTwo: "update README.md",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "netlify.toml",
+    textTwo: "added UI for signup & signin",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "package-lock.json",
+    textTwo: "Changed the base URL of the API.",
+    textThree: "5 months ago",
+  },
+  {
+    icon: "file",
+    textOne: "package.json",
+    textTwo: "Changed the base URL of the API.",
+    textThree: "5 months ago",
+  },
+];
---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const el = document.querySelector(".files-table");
+  info.forEach((item) => {
+    el.appendChild(new TableItem(item.textOne, item.textTwo, item.textThree));
+  });
+  el.appendChild(new TableItem());
+
+});
