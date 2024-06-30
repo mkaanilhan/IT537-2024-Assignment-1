2a361e33c3ced0ad4830dc81d86d1d49853c2a79
Eray Ermiş <eray.ermis@sabanciuniv.edu>
2024-06-30 10:40:43 +0300

    Changes completed

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ document.addEventListener("DOMContentLoaded", () => {
   info.forEach((item) => {
     el.appendChild(new TableItem(item.textOne, item.textTwo, item.textThree));
   });
-  el.appendChild(new TableItem());
 
 });
